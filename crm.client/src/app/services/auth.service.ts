219213c371541b7280b5fed4596bb86f2861d303
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-27 11:23:40 +0200

    Added auth.service, login and register models, register component.

---
+++ crm.client/src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RegisterModel, LoginModel } from '/../../../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:5000/api/auth';
+
+  constructor(private http: HttpClient) { }
+
+  register(model: RegisterModel): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, model);
+  }
+
+  login(model: LoginModel): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, model);
+  }
+}
