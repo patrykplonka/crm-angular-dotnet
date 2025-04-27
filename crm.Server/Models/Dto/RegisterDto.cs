c11a87a1551f858c54a6dcddcce73450e3a97aca
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-27 11:08:18 +0200

    Added Dto's models: Register and Login.

---
+++ crm.Server/Models/Dto/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace crm.Server.Models.Dto
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
