85af4773f6c797de5fb96cce95ac8eac3594a411
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-28 09:46:02 +0200

    Added dashboard and sidebar component.

--- crm.client/src/app/app-routing.module.ts
+++ crm.client/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './../app/components/register/register.component';
-import { LoginComponent } from './../app/components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
