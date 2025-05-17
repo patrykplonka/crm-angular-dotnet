3b642bdcb247928e1e18094af460ec4a06dc5b43
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-05-17 15:58:32 +0200

    Added Course model and Dto.

---
+++ crm.Server/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace crm.Server.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Instructor { get; set; }
+        public int DurationHours { get; set; }
+    }
+}
