4291f7c7e5a0f2b5c762b4d2ced491ff96945902
patrykplonka <patrykplonka2002@gmail.com>
2025-06-06 07:57:14 +0200

    Updated AppDbContext.

--- crm.Server/Data/ApplicationDbContext.cs
+++ crm.Server/Data/ApplicationDbContext.cs
@@ -13,5 +13,24 @@ namespace crm.Server.Data
 
         public DbSet<Course> Courses { get; set; }
         public DbSet<ScheduleEvent> Events { get; set; }
+        public DbSet<CourseEnrollment> CourseEnrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<CourseEnrollment>()
+                .HasKey(ce => ce.Id);
+
+            builder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.User)
+                .WithMany()
+                .HasForeignKey(ce => ce.UserId);
+
+            builder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.Course)
+                .WithMany()
+                .HasForeignKey(ce => ce.CourseId);
+        }
     }
 }
